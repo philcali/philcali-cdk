f77e4f8a102fff29de63c3cf23cb20ac24be3a5b
Philip Cali <philip.cali@gmail.com>
2022-02-05 16:21:56 -0500

    feat: adding devlib

--- pipeline/bin/pipeline.ts
+++ pipeline/bin/pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from 'monocdk';
 import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', {
+new PipelineStack(app, 'CDKConstrictsPipelineStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- pipeline/lib/pipeline-stack.ts
+++ pipeline/lib/pipeline-stack.ts
@@ -1,16 +1,36 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps } from 'monocdk';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as delivlib from 'aws-delivlib';
+import { BuildSpec } from 'monocdk/lib/aws-codebuild';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new delivlib.Pipeline(this, 'ConsutructsPipeline', {
+        repo: new delivlib.GitHubRepo({
+            repository: 'philcali/philcali-cdk',
+            tokenSecretArn: 'GITHUB_TOKEN_NAME'
+        }),
+        buildSpec: BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            install: {
+              commands: [
+                'cd device-lab',
+                'npm install'
+              ]
+            },
+            build: {
+              commands: [
+                'npm run build'
+              ]
+            }
+          },
+          artifacts: {
+            'base-directory': 'device-lab/dist'
+          }
+        })
+    });
   }
 }
