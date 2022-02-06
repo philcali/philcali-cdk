2234c6c6e6882ad5a3642c99d6a534aaca5ee155
Philip Cali <philip.cali@gmail.com>
2022-02-06 14:53:58 -0500

    feat: first pass delivery pipeline

---
+++ pipeline/lib/publishing/s3/publish.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -euo pipefail
+echo ----------------------------------------
+echo "Sources:"
+ls
+echo ----------------------------------------
+
+# Sync to S3 publicly readable
+args=""
+if ${PUBLIC:-false}; then
+  args="--acl public-read"
+fi
+
+idempotency_token=""
+
+# See if there's a file with publishing commands
+if [[ -f s3-publishing.json ]]; then
+    echo "Found publishing instructions"
+    cat s3-publishing.json
+
+    idempotency_token=$(node -pe "require('./s3-publishing.json')['idempotency-token'] || ''")
+
+    # We don't want to upload this file
+    args="$args --exclude s3-publishing.json"
+fi
+
+if [[ "${idempotency_token:-}" != "" ]]; then
+    echo "Idempotency token: $idempotency_token"
+
+    # Must use 's3 cp' to try and read exact filename. 's3 ls' would match prefixes as well.
+    aws s3 cp $BUCKET_URL/$idempotency_token - > /dev/null 2>&1 && {
+        echo "Token found, stopping."
+        exit 0
+    } || {
+        echo "Idempotency token not found, continuing."
+    }
+fi
+
+# Do the copy
+echo "Starting the upload to $BUCKET_URL"
+echo "Source directory $SOURCE_DIRECTORY"
+echo "(Args: $args)"
+
+if $FOR_REAL; then
+    cd $SOURCE_DIRECTORY
+    aws s3 cp --recursive . $BUCKET_URL $args
+
+    if [[ "${idempotency_token:-}" != "" ]]; then
+        echo "Writing idempotency token..."
+        echo 1 | aws s3 cp - $BUCKET_URL/$idempotency_token
+    fi
+else
+    echo "==========================================="
+    echo "            🏜️ DRY-RUN MODE 🏜️"
+    echo
+    echo "Skipping the actual publishing step."
+    echo
+    echo "Set FOR_REAL=true to do it!"
+    echo "==========================================="
+fi
--- pipeline/test/pipeline.test.ts
+++ pipeline/test/pipeline.test.ts
@@ -1,17 +1,16 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Pipeline from '../lib/pipeline-stack';
+import * as cdk from 'monocdk';
+import { Template } from 'monocdk/assertions';
+import * as pipeline from '../lib/pipeline-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/pipeline-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Pipeline.PipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+    const app = new cdk.App();
+    // WHEN
+    const stack = new pipeline.PipelineStack(app, 'MyTestStack');
+    // THEN
+    const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    template.hasResourceProperties('AWS::CodePipeline::Pipeline', {
+    });
 });
